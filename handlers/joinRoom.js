00ed9970633745cea7a702bae7dfc85cd788321f
ananyamahesh511 <ananyamahesh006@gmail.com>
2025-10-04 16:01:26 +0530

    added user status functionality

--- handlers/joinRoom.js
+++ handlers/joinRoom.js
@@ -1,9 +1,18 @@
 const ChatRoom = require('../models/Chatroom').ChatRoom;
+const User = require('../models/User');
 
 module.exports = async (io, socket, data) => {
     try{
+      const {roomId, userId} = data; 
+
       socket.join(roomId);
       console.log(`${socket.id} joined room ${roomId}`);
+
+      if (userId) {
+        socket.data.userId = userId;
+        await User.findOneAndUpdate({userId}, {online: true});
+        io.emit("userStatusChanged", { userId, online: true });
+      }
 
       //finding the chatroom and returning messages
       const chatRoom = await ChatRoom.findOne({roomId}).populate("users");
@@ -14,6 +23,14 @@ module.exports = async (io, socket, data) => {
 
       //sending all messages stored in chat room
       socket.emit("chatHistory", chatRoom.messages);
+
+      socket.on("disconnect", async() => {
+        if(socket.data.userId) {
+            await User.findOneAndUpdate({userId: socket.data.userId}, {online: false});
+            io.emit("userStatusChanged", { userId: socket.data.userId, online: false });
+            console.log(`User ${socket.data.userId} went offline`);
+        }
+      });
     } catch (err) {
       console.error("Error fetching chatroom messages:", err);
       socket.emit("chatHistory", []);
