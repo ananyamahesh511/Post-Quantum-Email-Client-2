bc632b73415c541107b7e8288f092daa2c6c0a16
ananyamahesh511 <ananyamahesh006@gmail.com>
2025-10-04 17:14:50 +0530

    changes made

--- routes/client/permissions.js
+++ routes/client/permissions.js
@@ -26,8 +26,8 @@ module.exports = {
 
     async post(req, res) {
         try{
-            const {userId} = req.params;
-            const {field} = req.body;
+            // const {userId} = req.params;
+            const {field, userId, status} = req.body;
 
             if (!["isExports", "isScreenshots"].includes(field)) {
                 return res.status(400).json({ error: "Invalid field to toggle" });
@@ -38,14 +38,14 @@ module.exports = {
                 return res.status(404).json({ error: "User not found" });
             }
 
-            user[field] = !user[field];
+            user[field] = status;
             await user.save();
 
             res.json({
                 isExports: user.isExports,
                 isScreenshots: user.isScreenshots
             });
-            
+
         } catch (error) {
             console.error("Error toggling permissions:", error);
             res.status(500).json({ error: "Internal server error" });
--- routes/client/users.js
+++ routes/client/users.js
@@ -1,24 +1,29 @@
 const User = require('../../models/User');
+const ShortUniqueId = require('short-unique-id');
+const uid = new ShortUniqueId({ length: 5 });
 
 module.exports = {
 	name: 'users',
 	async post(req, res) {
 		try {
-            const { userId, name, email } = req.body;
-            if (!userName || !name || !email) {
-                return res.status(400).json({ error: "Username and emai required" });
+            const userId = uid.rnd();
+
+            const { name, email, phone } = req.body;
+            if (!name || !email || !phone) {
+                return res.status(400).json({ error: "Name and email required" });
             }
 
-            const existingUser = await User.findOne({ $or: [{ userId }, { email }] });
+            const existingUser = await User.findOne({ $or: [ { email }, { phone }] });
             if (existingUser) {
                 return res.status(400).json({ error: "User already exists" });
             }
 
-            const newUser = new User({ userId, name, email });
+            const newUser = new User({ userId, name, email, phone });
             await newUser.save();
             res.status(201).json({ message: "User created successfully", user: newUser });
         } catch (err) {
             console.error("Error creating user:", err);
+            res.status(500).json({ error: "Internal server error" });
         }
 	},
 
